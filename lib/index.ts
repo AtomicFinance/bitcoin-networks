d8b25c4bb41fde4e391cafbf4f7d5b11c08c10ca
Matthew Black <mblackmblackmblack@gmail.com>
2025-08-28 14:20:44 -0400

    dep-up: bitcoinjs-lib 6.1.7

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-import { Network as BitcoinJsLibNetwork, networks } from 'bitcoinjs-lib';
+import { Network as BitcoinJsLibNetwork, networks } from "bitcoinjs-lib";
 
 export interface Network {
   name: string;
@@ -9,23 +9,23 @@ export interface Network {
 export interface BitcoinNetwork extends Network, BitcoinJsLibNetwork {}
 
 const bitcoin: BitcoinNetwork = {
-  name: 'bitcoin',
+  name: "bitcoin",
   ...networks.bitcoin,
-  coinType: '0',
+  coinType: "0",
   isTestnet: false,
 };
 
 const bitcoin_testnet: BitcoinNetwork = {
-  name: 'bitcoin_testnet',
+  name: "bitcoin_testnet",
   ...networks.testnet,
-  coinType: '1',
+  coinType: "1",
   isTestnet: true,
 };
 
 const bitcoin_regtest: BitcoinNetwork = {
-  name: 'bitcoin_regtest',
+  name: "bitcoin_regtest",
   ...networks.regtest,
-  coinType: '1',
+  coinType: "1",
   isTestnet: true,
 };
 
@@ -39,22 +39,13 @@ export { BitcoinNetworks };
 
 export const chainHashFromNetwork = (network: BitcoinNetwork): Buffer => {
   switch (network.name) {
-    case 'bitcoin':
-      return Buffer.from(
-        '6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000',
-        'hex',
-      );
-    case 'bitcoin_testnet':
-      return Buffer.from(
-        '43497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea330900000000',
-        'hex',
-      );
-    case 'bitcoin_regtest':
-      return Buffer.from(
-        '06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f',
-        'hex',
-      );
+    case "bitcoin":
+      return Buffer.from("6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000", "hex");
+    case "bitcoin_testnet":
+      return Buffer.from("43497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea330900000000", "hex");
+    case "bitcoin_regtest":
+      return Buffer.from("06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f", "hex");
     default:
-      throw Error('Provided network not supported');
+      throw Error("Provided network not supported");
   }
 };
